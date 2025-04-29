cc8a060e8e7173f4ce0efeb4c716fda266e5c565
Marcos <exegeses@gmail.com>
2025-04-29 10:50:22 -0300

    material práctica fórmula 1

---
+++ clase6/js/eventos.js
@@ -0,0 +1,29 @@
+// Ubicamos elementos dentro del DOMO
+const btn = document.querySelector('#btn')
+const contenedor = document.querySelector('#contenedor')
+
+// addEventListener( 'evento', acción )
+
+btn.addEventListener( 
+        'click', 
+        function()
+        {
+            contenedor.textContent = '¡hiciste click!'
+        } 
+    )
+
+btn.addEventListener(
+        'mouseover',
+        function()
+        {
+            contenedor.textContent = 'mouse sobre'
+        }
+    )    
+
+btn.addEventListener(
+        'mouseout',
+        function()
+        {
+            contenedor.textContent = 'reposo'
+        }
+    )
